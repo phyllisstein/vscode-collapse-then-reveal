4568d0699cdeefbb9d17a61ed1a904f39b7b111d
Daniel P. Shannon <daniel@daniel.sh>
2020-05-13 11:43:30 -0400

    🎨 Fix formatting issues (tabs!).

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Collapse Then Reveal for Visual Studio Code
-`collapse-then-reveal` is a Visual Studio Code extension to keep your workspace neat and tidy. Every time your active file changes, it collapses the Explorer and focuses the tree directly on what you're doing. Comme ça:
+`collapse-then-reveal` is a Visual Studio Code extension to keep your workspace
+neat and tidy. Every time your active file changes, it collapses inactive directories in the Explorer so you can focus on what you're doing.
+
+Comme ça:
 
 ![](demo.gif)
--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,18 @@
 import {
-	commands,
-	ExtensionContext,
-	TextEditor,
-	window,
+  commands,
+  ExtensionContext,
+  TextEditor,
+  window,
 } from 'vscode'
 
 export function activate(context: ExtensionContext) {
-	context.subscriptions.push(window.onDidChangeVisibleTextEditors(collapseThenReveal))
+  context.subscriptions.push(window.onDidChangeVisibleTextEditors(collapseThenReveal))
 }
 
 async function collapseThenReveal(editors?: TextEditor[]) {
-	if (editors && editors.length) {
-		await commands.executeCommand('workbench.files.action.collapseExplorerFolders')
-		await commands.executeCommand('revealInExplorer')
-		window.showTextDocument(editors[0].document)
-	}
+  if (editors && editors.length) {
+    await commands.executeCommand('workbench.files.action.collapseExplorerFolders')
+    await commands.executeCommand('revealInExplorer')
+    window.showTextDocument(editors[0].document)
+  }
 }
