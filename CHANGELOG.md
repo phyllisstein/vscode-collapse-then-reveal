22562d962d91a4af375c4f91ee85376b35eed085
Daniel P. Shannon <daniel@daniel.sh>
2020-05-13 19:15:27 -0400

    🐛 Don't steal focus from non-file editors.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 # Change Log
+## 0.0.3 - 13 May 2020
+* Fix focus-stealing bug by ignoring editors which are not bound to a discrete
+  file.
+
 ## 0.0.2 - 7 May 2019
 * Correctly `await` asynchronous workbench commands.
 
--- src/extension.ts
+++ src/extension.ts
@@ -4,13 +4,15 @@ import {
   TextEditor,
   window,
 } from 'vscode'
+import debounce from 'lodash.debounce'
+import head from 'lodash.head'
 
 export function activate(context: ExtensionContext) {
-  context.subscriptions.push(window.onDidChangeVisibleTextEditors(collapseThenReveal))
+  context.subscriptions.push(window.onDidChangeVisibleTextEditors(debounce(collapseThenReveal, 0, { leading: false, trailing: true })))
 }
 
 async function collapseThenReveal(editors?: TextEditor[]) {
-  if (editors && editors.length) {
+  if (editors && editors.length && head(editors)!.document.uri.scheme === 'file') {
     await commands.executeCommand('workbench.files.action.collapseExplorerFolders')
     await commands.executeCommand('revealInExplorer')
     window.showTextDocument(editors[0].document)
